c72ef3badc7c4bf6d96cbd34710c0232d270d2ff
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 11:38:10 +0700

    pwa 10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default App;
