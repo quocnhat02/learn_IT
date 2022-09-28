f16168827101a894545a3a0cc0c8704527a18f27
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 11:34:43 +0700

    pwa 8

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -4,11 +4,15 @@ const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
 const fetchWeather = async (query) => {
-  const response = await axios.get(URL, {
+  const { data } = await axios.get(URL, {
     params: {
       q: query,
       units: 'metric',
       APPID: API_KEY,
     },
   });
+
+  return DataTransfer;
 };
+
+export { fetchWeather };
