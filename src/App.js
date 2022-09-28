7cb24918bd3aec746a472b8e238dcec68a37fd21
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 11:55:17 +0700

    pwa 15

--- src/App.js
+++ src/App.js
@@ -32,6 +32,10 @@ const App = () => {
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
         </div>
       )}
     </div>
