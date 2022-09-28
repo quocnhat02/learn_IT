16dd7b2763da1ccbbced5b8835a6f1b9572b0c5e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 11:57:20 +0700

    pwa 16

--- src/App.js
+++ src/App.js
@@ -36,6 +36,13 @@ const App = () => {
             {Math.round(weather.main.temp)}
             <sup>&deg;C</sup>
           </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+          </div>
         </div>
       )}
     </div>
