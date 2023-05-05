112505e8c2e08de9249a7a1275137e30db97bd9b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-05 22:23:43 +0700

    react 3

--- reactvitecourse/src/App.jsx
+++ reactvitecourse/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <Counter check={check} />
+      <Counter check={check} setCheck={setCheck} />
     </div>
   );
 }
--- reactvitecourse/src/Counter.jsx
+++ reactvitecourse/src/Counter.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react';
 
-const Counter = ({ check }) => {
+const Counter = ({ check, setCheck }) => {
   const [counter, setCounter] = useState(0);
 
   const handleIncrement = () => {
     setCounter((counter) => counter + 1);
+    // if (counter >= 5) setCheck('You clicked enough');
   };
 
   return (
     <div>
-      {counter < 5 ? (
-        <h1>Counter is too low</h1>
-      ) : (
-        <h1>Counter is too high</h1>
-      )}
-      <h1>{counter}</h1>
+      {counter > 5 && <h2>Yeahhhhhhh</h2>}
+      {counter}
       <button onClick={handleIncrement}>Increment</button>
     </div>
   );
