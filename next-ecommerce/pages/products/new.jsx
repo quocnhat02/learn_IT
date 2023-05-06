9d1e075edb7a0821e7d40fd726c380e6c4c944fe
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-06 18:54:00 +0700

    new product 3

--- next-ecommerce/pages/products/new.jsx
+++ next-ecommerce/pages/products/new.jsx
@@ -6,30 +6,40 @@ const NewProduct = () => {
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
 
+  const createProduct = (e) => {
+    e.preventDefault();
+    const data = { title, description, price };
+    axios.post('/api/products', data);
+  };
+
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label>Product name</label>
-      <input
-        type='text'
-        placeholder='product name'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <label>Description</label>
-      <textarea
-        placeholder='description'
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <label>Price (in USE)</label>
-      <input
-        type='text'
-        placeholder='price'
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <button className='btn-primary'>Save</button>
+      <form action='' onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product name</label>
+        <input
+          type='text'
+          placeholder='product name'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>Price (in USE)</label>
+        <input
+          type='text'
+          placeholder='price'
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type='submit' className='btn-primary'>
+          Save
+        </button>
+      </form>
     </Layout>
   );
 };
