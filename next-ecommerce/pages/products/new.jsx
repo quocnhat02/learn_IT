5766ba8a31f73a4c3cf768cb8dd51f3b860cb0bc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-06 18:46:54 +0700

    new product 1

--- next-ecommerce/pages/products/new.jsx
+++ next-ecommerce/pages/products/new.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
 import Layout from '../../components/Layout';
 
 const NewProduct = () => {
-  return <Layout>NewProduct</Layout>;
+  return (
+    <Layout>
+      <h1>New Product</h1>
+      <label>Product name</label>
+      <input type='text' placeholder='product name' />
+      <label>Description</label>
+      <textarea placeholder='description'></textarea>
+      <label>Price (in USE)</label>
+      <input type='text' placeholder='price' />
+      <button className='btn-primary'>Save</button>
+    </Layout>
+  );
 };
 
 export default NewProduct;
