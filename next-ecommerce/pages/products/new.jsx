5a9f3db628629e4a2cb0f7090fe41044e23c5a62
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-06 18:49:30 +0700

    new product 2

--- next-ecommerce/pages/products/new.jsx
+++ next-ecommerce/pages/products/new.jsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import Layout from '../../components/Layout';
 
 const NewProduct = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+
   return (
     <Layout>
       <h1>New Product</h1>
       <label>Product name</label>
-      <input type='text' placeholder='product name' />
+      <input
+        type='text'
+        placeholder='product name'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <label>Description</label>
-      <textarea placeholder='description'></textarea>
+      <textarea
+        placeholder='description'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <label>Price (in USE)</label>
-      <input type='text' placeholder='price' />
+      <input
+        type='text'
+        placeholder='price'
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
       <button className='btn-primary'>Save</button>
     </Layout>
   );
