347894ea029faee0d4b9656e7c029975f5975738
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-06 18:35:58 +0700

    new product

--- next-ecommerce/pages/products.jsx
+++ next-ecommerce/pages/products.jsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
 import Layout from '../components/Layout';
 
 const Products = () => {
-  return <Layout>Products</Layout>;
+  return (
+    <Layout>
+      <Link
+        className='bg-blue-900 text-white rounded-md py-1 px-2'
+        href={'/products/new'}
+      >
+        Add new product
+      </Link>
+    </Layout>
+  );
 };
 
 export default Products;
