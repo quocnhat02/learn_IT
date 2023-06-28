13d9610333e2158fa750eba3e5d0cecf6d12beeb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-28 21:32:37 +0700

    error handle

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@ app.use(
 // use middleware to handle cors policy
 app.use(cors());
 
+// health point or actuator
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Service is up',
@@ -21,5 +22,20 @@ app.get('/', (req, res, next) => {
 });
 
 // router
+// app.use('/register', registrationRouter)
+
+// bad url or error we can handle
+// with middleware
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: { message: error.message, status: error.status },
+  });
+});
 
 module.exports = app;
