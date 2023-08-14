afd3cc8676c4d22dbf1c7aa81cef8e8a28b49a8e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-14 17:28:40 +0700

    create the request listener

---
+++ app/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// use middleware to form our contract for incoming json payloads ONLY!
+app.use(express.json());
+// use middleware for url encoding
+app.use(express.urlencoded({ extended: true }));
+// use middleware to handle cors policy
+app.use(cors());
+
+// health point or actuator
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Service is up',
+  });
+});
+
+// routers
+// app.use("/register", registrationRouter)
+// bad url or error we can handle
+// with middleware
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    status: err.status,
+  });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const app = require('./app/app');
 require('dotenv').config({});
 
 const PORT = process.env.PORT;
 
-http.createServer().listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
