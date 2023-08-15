9fc8b93328d9328d2df836293545e1d93298df1d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-15 16:03:48 +0700

    create database functional

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const User = require('../model/userModel');
 
 const connect = () => {
   mongoose
@@ -12,4 +13,16 @@ const connect = () => {
     });
 };
 
-module.exports = { connect };
+const disconnect = async () => {
+  await mongoose.connection.close();
+};
+
+const findUser = async (obj) => {
+  return await User.findOne(obj);
+};
+
+const saveUser = async (newUser) => {
+  return await newUser.save();
+};
+
+module.exports = { connect, disconnect, findUser, saveUser };
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,59 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'successful - GET',
-    metadata: {
-      hostname: req.hostname,
-      method: req.method,
-    },
-  });
-});
+router.post('/register', (req, res, next) => {});
 
-router.get('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'successful - GET by id',
-    metadata: {
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method,
-    },
-  });
-});
-
-router.post('/', (req, res, next) => {
-  const name = req.body.name;
-  res.status(200).json({
-    message: 'successful - POST by id',
-    metadata: {
-      name,
-      hostname: req.hostname,
-      method: req.method,
-    },
-  });
-});
-
-router.put('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'successful - PUT by id',
-    metadata: {
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method,
-    },
-  });
-});
-
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'successful - DELETE by id',
-    metadata: {
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method,
-    },
-  });
-});
+router.post('/login', (req, res, next) => {});
 
 module.exports = router;
